f215708b2ca08c19a94a8785f1a0850dc33e5e6c
AmolPWagh <102019595+AmolPWagh@users.noreply.github.com>
2026-04-27 09:09:52 +0530

    Time: 1 ms (80.07%) | Memory: 42.6 MB (23.35%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static int sumOfSquares(int num){
+        if(num==0) return 0;
+        int m = num%10;
+        return m*m + sumOfSquares(num/10);
+    }
+    public boolean isHappy(int n) {
+
+        int slow = n;
+        int fast = n;
+
+        do{
+            slow = sumOfSquares(slow);
+            fast = sumOfSquares(sumOfSquares(fast));
+            if (fast == 1){
+                return true;
+            }
+        }
+        while(slow!=fast);
+
+        return false;
+    }
+}
