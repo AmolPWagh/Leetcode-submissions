4e44651f503bb585d03b017f759dd267509f8c5d
AmolPWagh <102019595+AmolPWagh@users.noreply.github.com>
2026-04-27 09:20:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (67.97%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,9 +1,20 @@
 class Solution {
+    // public static int sumOfSquares(int num){
+    //     if(num==0) return 0;
+    //     int m = num%10;
+    //     return m*m + sumOfSquares(num/10);
+    // }
+
     public static int sumOfSquares(int num){
-        if(num==0) return 0;
-        int m = num%10;
-        return m*m + sumOfSquares(num/10);
+        int ans = 0;
+        while(num>0){
+            int m = num%10;
+            num = num/10;
+            ans += m*m;
+        }
+        return ans;
     }
+
     public boolean isHappy(int n) {
 
         int slow = n;
