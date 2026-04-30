7c2ea6e94e15a5f7134616a357df3e90de478b77
AmolPWagh <102019595+AmolPWagh@users.noreply.github.com>
2026-04-30 10:20:31 +0530

    Time: 1 ms (9.08%) | Memory: 48.6 MB (14.31%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,11 +1,21 @@
 class Solution {
 
-    static int helper(int[] nums, int target, int index){
-        if (index==nums.length) return -1;
-        if (nums[index]==target)return index;
-        return helper(nums, target, ++index);
-    }
+    //using recursion
+
+    // static int helper(int[] nums, int target, int index){
+    //     if (index==nums.length) return -1;
+    //     if (nums[index]==target)return index;
+    //     return helper(nums, target, ++index);
+    // }
+    // public int search(int[] nums, int target) {
+    //     return helper(nums, target, 0);
+    // }
+
+    // using iteration
     public int search(int[] nums, int target) {
-        return helper(nums, target, 0);
+        for (int i=0; i<nums.length; i++){
+            if (nums[i]==target) return i;
+        }
+        return -1;
     }
 }
