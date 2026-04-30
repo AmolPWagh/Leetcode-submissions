50bb4471122f1311c6fca167e18942347fac5a08
AmolPWagh <102019595+AmolPWagh@users.noreply.github.com>
2026-04-30 10:43:33 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (31.65%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -2,26 +2,29 @@ class Solution {
 
     //using recursion
 
-    // static int helper(int[] nums, int target, int index){
-    //     if (index==nums.length) return -1;
-    //     if (nums[index]==target)return index;
-    //     return helper(nums, target, ++index);
-    // }
-    // public int search(int[] nums, int target) {
-    //     return helper(nums, target, 0);
-    // }
+    static int helper(int[] nums, int target, int s, int e){
+        
+        if (s>e)return -1;
+        int m = s + (e-s)/2;
+        if (nums[m]== target) return m;
+        else if(nums[m]>target) return helper(nums, target, s, m-1);
+        return helper(nums, target, m+1, e);
+    }
+    public int search(int[] nums, int target) {
+        return helper(nums, target, 0, nums.length-1);
+    }
 
     // using iteration
-    public int search(int[] nums, int target) {
-        int s=0;
-        int e=nums.length-1;
+    // public int search(int[] nums, int target) {
+    //     int s=0;
+    //     int e=nums.length-1;
         
-        while(s<=e){
-            int mid = s + (e-s)/2;
-            if (nums[mid]==target) return mid;
-            else if (nums[mid]>target) e=mid-1;
-            else s=mid+1;
-        }
-        return -1;
-    }
+    //     while(s<=e){
+    //         int mid = s + (e-s)/2;
+    //         if (nums[mid]==target) return mid;
+    //         else if (nums[mid]>target) e=mid-1;
+    //         else s=mid+1;
+    //     }
+    //     return -1;
+    // }
 }
