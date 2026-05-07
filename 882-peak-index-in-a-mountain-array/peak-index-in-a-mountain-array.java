4cffe04a6a357d02d7a4ff2959b2c44efd5d8467
AmolPWagh <102019595+AmolPWagh@users.noreply.github.com>
2026-05-07 11:44:52 +0530

    Time: 0 ms (100.00%) | Memory: 80.3 MB (57.64%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -1,13 +1,22 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int s=0;
-        int e=arr.length-1;
+        int s = 0;
+        int e = arr.length-1;
 
         while(s<e){
             int mid = s + (e-s)/2;
-            if(arr[mid] > arr[mid+1]) e=mid;
-            else s=mid+1;
+            if (arr[mid] > arr[mid+1]){
+                // you are in dec part of array
+                // this may be ans, but look at left
+                // this is why e != mid-1;
+                e = mid;
+            }
+            else{
+                // you are in asc part of array
+                s = mid+1; // bcuz mid+1 ele >mid ele
+            }
         }
+        // int the end s==e and pointing to largest element in array
         return(s);
     }
 }
