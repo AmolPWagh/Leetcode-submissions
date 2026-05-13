df072fae1073293184a189b1c4e46763600bd608
AmolPWagh <102019595+AmolPWagh@users.noreply.github.com>
2026-05-14 00:32:08 +0530

    Time: 3 ms (81.62%) | Memory: 46.5 MB (66.99%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -0,0 +1,36 @@
+class Solution {
+    static void swap( char[] arr, int i, int j){
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static boolean isVowel(char c){
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
+
+    public String reverseVowels(String s) {
+        char[] arr = s.toCharArray();
+        int i=0;
+        int j=arr.length-1;
+        while(i<j){
+            if ( isVowel(arr[i]) && !isVowel(arr[j])) {
+                j--;
+            }
+            if ( !isVowel(arr[i]) && isVowel(arr[j])) {
+                i++;
+            }
+            if ( !isVowel(arr[i]) && !isVowel(arr[j])) {
+                i++;
+                j--;
+            }
+            if ( isVowel(arr[i]) && isVowel(arr[j])) {
+                swap (arr, i, j);
+                i++;
+                j--;
+            }
+
+        }
+        return new String(arr);
+    }
+}
