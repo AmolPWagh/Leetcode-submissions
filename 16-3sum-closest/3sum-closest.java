5ff836cbfb44fa50d5e109e7f5c4c43422f8a65a
AmolPWagh <102019595+AmolPWagh@users.noreply.github.com>
2026-05-14 23:36:25 +0530

    Time: 18 ms (41.40%) | Memory: 45.6 MB (30.57%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -3,7 +3,6 @@ class Solution {
         Arrays.sort(nums);
         int sum = nums[0] + nums[1] + nums[2];
         for (int i=0; i<nums.length-2; i++){
-            if (i>0 && nums[i] == nums[i-1]) continue;
 
             int j = i+1;
             int k = nums.length-1;
@@ -14,11 +13,9 @@ class Solution {
 
                 if (currSum < target){
                     j++;
-                    while(j<k && nums[j]==nums[j-1]) j++;
                 }
                 else if (currSum > target){
                     k--;
-                    while(j<k && nums[k]==nums[k+1]) k--;
                 }
                 else{
                    return  currSum;
